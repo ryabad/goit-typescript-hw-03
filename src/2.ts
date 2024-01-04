b3f848a0fa6f5c464b5c7f39ab05c73e7e0d9bf2
ryabad <ryabaDa@gmail.com>
2024-01-04 22:39:03 +0200

    init

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,22 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  // public name: string;
+  // private department: string;
+  // protected salary: number;
+
+  // constructor(name: string, department: string, salary: number) {
+  //   this.name = name;
+  //   this.department = department;
+  //   this.salary = salary;
+  // }
+
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -33,7 +40,20 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    //super(name, department, salary + 1000)
+  }
+
+  increaseSalary() {
+    this.salary += 1000;
+  }
 }
 
+//testing
+const human = new Manager("Dima", "test", 1000);
+human.increaseSalary();
+human.increaseSalary();
+console.log("Employee info", human.getEmployeeDetails());
 
-export {};
+export { Manager };
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(
+        `Person used this key (${person
+          .getKey()
+          .getSignature()}) entered the house`
+      );
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened!");
+    } else {
+      console.log(`Invalid key`);
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +66,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
